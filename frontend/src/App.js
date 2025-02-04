74e4eeb9e1df70696e8ad69e41d7143d6cc68b6e
Soubhagya Ranjan Sethi <93478954+soubhagyagithub@users.noreply.github.com>
2025-02-04 22:35:50 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Chats from "./pages/Chats";
+
+function App() {
+  return (
+    <div className="App">
+      <Route path="/" component={Home} exact />
+      <Route path="/chats" component={Chats} />
+    </div>
+  );
+}
+
+export default App;
